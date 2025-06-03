a6bfad63831ddac4f976f1999bc2c2c33d52a88c
anan <dingwuying@totanota.com>
2025-06-03 19:30:36 +0300

    Install eslint and apply configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+  {
+    files: ['**/*.js', '**/*.ts', '*.mjs'],
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/jsx-quotes': ['error', 'prefer-single'],
+      '@stylistic/comma-spacing': 'error',
+      '@stylistic/indent': ['error', 2],
+      'eqeqeq': 'error',
+      '@stylistic/block-spacing': 'error',
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/arrow-spacing': 'error',
+      '@stylistic/keyword-spacing': ['error', { before: true }],
+    },
+  },
+];
