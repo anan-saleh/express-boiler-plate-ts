b4e4e3e956e8fb3c33a25d340050c178ae390fd0
anan <dingwuying@totanota.com>
2025-06-04 10:23:44 +0300

    Isolare env.ts file on util from every import to avoid conflict and add NODE_ENV variable

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,18 +1,19 @@
-import { log, LogLevel } from "./logger";
-
+// isolate this file from every import there is since it will be used everywhere
+// inorder to avoid conflict
 export const getEnvVariable = (key: string): string | undefined => {
   const value = process.env[key];
   if (!value) {
-    log(`Missing environment variable ${key}`, LogLevel.ERROR);
+    console.error(`Missing environment variable ${key}`);
   } else {
     return value;
   }
-}
+};
 
 const MONGO_COLLECTION = 'SomeCollection';
 
 export const ENV = {
   PORT: parseInt(String(getEnvVariable('PORT'))),
-  LOG_LEVEL: getEnvVariable('LOG_LEVEL'),
+  LOG_LEVEL: getEnvVariable('LOG_LEVEL') || 'info',
   MONGO_URI: getEnvVariable('MONGO_URI') || `mongodb://127.0.0.1:27017/${MONGO_COLLECTION}`,
+  NODE_ENV: getEnvVariable('NODE_ENV') || 'production',
 };
