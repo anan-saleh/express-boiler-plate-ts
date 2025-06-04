d25f6593cf46703d826e4874be5ec0a14950618f
anan <dingwuying@totanota.com>
2025-06-04 11:20:12 +0300

    Add handlers for un caught exception and unhandled rejections

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,16 @@ import { ENV } from './utils/env';
 import { log } from './utils/logger';
 import { connectDB } from './config/db';
 
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
 const PORT = ENV.PORT;
 const startServer = () => {
   connectDB().then(() => {
