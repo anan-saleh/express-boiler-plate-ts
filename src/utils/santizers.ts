677a8d73d929777e158dde380b81d8e2cee0a80f
anan <dingwuying@totanota.com>
2025-06-05 13:00:11 +0300

    Extend the sanitizer to work with toObject or regular object of a userDocument

--- src/utils/santizers.ts
+++ src/utils/santizers.ts
@@ -1,6 +1,8 @@
 import { UserDocument } from '../models/user.model';
+import { isFunction } from './helpers';
 
 export const sanitizeUser = (user: UserDocument) => {
-  const { password, __v, comparePassword,  ...safeUser} = user.toObject();
+  const notSafeUser = isFunction(user?.toObject) ? user.toObject() : user;
+  const { password, __v, comparePassword, sanitize,  ...safeUser} = notSafeUser;
   return safeUser;
 };
