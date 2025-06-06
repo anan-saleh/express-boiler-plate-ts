8b9fbccb28d61265caab35188f4a208367442dc9
anan <dingwuying@totanota.com>
2025-06-06 18:40:42 +0300

    Install helmet and apply it as middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import './config/env';
 import express from 'express';
+import helmet from 'helmet';
 import session from 'express-session';
 import passport from 'passport';
 import './config/passport/index';
 import { ENV } from './utils/env';
+import authRouter from './routes/auth.routes';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app  = express();
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
