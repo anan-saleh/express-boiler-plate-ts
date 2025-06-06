c926c40ccba722ca24a3392493dd41ac391be800
anan <dingwuying@totanota.com>
2025-06-06 07:57:36 +0300

    Apply catchAsync function on auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,34 +3,23 @@ import { HTTP_STATUS } from '../utils/httpStatus';
 import { authenticateUser, logoutUserSession } from '../services/auth.service';
 import { createUser } from '../services/user.service';
 import { RegisterInput, LoginInput } from '../schemas/auth.schema';
+import { catchAsync } from '../utils/catchAsync';
 
-export const registerUser = async (req: Request<{}, {}, RegisterInput>, res: Response, next: NextFunction) => {
-  try {
-    const user = await createUser(req.body);
-    res.status(HTTP_STATUS.CREATED).json({ message: 'User registered successfully', user });
-  } catch (err) {
-    next(err);
-  }
-};
+export const registerUser = catchAsync(async (req: Request<{}, {}, RegisterInput>, res: Response, next: NextFunction) => {
+  const user = await createUser(req.body);
+  res.status(HTTP_STATUS.CREATED).json({ message: 'User registered successfully', user });
+});
 
-export const loginUser = async (req: Request<{}, {}, LoginInput>, res: Response, next: NextFunction) => {
-  try {
-    const user = await authenticateUser(req);
-    res.status(HTTP_STATUS.OK).json({
-      message: 'Login successful',
-      user: user,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+export const loginUser = catchAsync(async (req: Request<{}, {}, LoginInput>, res: Response, next: NextFunction) => {
+  const user = await authenticateUser(req);
+  res.status(HTTP_STATUS.OK).json({
+    message: 'Login successful',
+    user: user,
+  });
+});
 
-export const logoutUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await logoutUserSession(req);
-    res.clearCookie('connect.sid');
-    res.status(HTTP_STATUS.OK).json({ message: 'Logged out successfully' });
-  } catch (err) {
-    next(err);
-  }
-};
+export const logoutUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  await logoutUserSession(req);
+  res.clearCookie('connect.sid');
+  res.status(HTTP_STATUS.OK).json({ message: 'Logged out successfully' });
+});
